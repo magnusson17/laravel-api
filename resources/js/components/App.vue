6c9bc7baa73ebc9eee799d3aa13286844cf8db1f
Ga <gabri.magni.97@gmail.com>
2022-06-15 15:09:00 +0200

    first commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+
+        <Header/>
+        <main class="container">
+            <!-- <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <Header></Header>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div> -->
+
+            <div class="container">
+
+            </div>
+        </main>
+    </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+
+export default {
+    name: "App",
+    components: {
+        Header
+    }
+}
+
+</script>
